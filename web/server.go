c6bd16dd0f58693d6a43be815a9244fd1469f8db
Mario Castro <mariocaster@gmail.com>
2016-02-19 21:22:59 +0100

    First idea of server with some hardcoding yet

---
+++ web/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/sayden/gubsub/queue"
+)
+
+//Start will run the web server
+func Start(q queue.Stacker) {
+	http.HandleFunc("/receive", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, q)
+	})
+	http.ListenAndServe(":12345", nil)
+}
+
+//Parses a request to return a queue.Message object
+func getMessageFromRequest(r *http.Request) (*queue.Message, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := r.Body.Close(); err != nil {
+		return nil, err
+	}
+
+	msg := queue.Message{Data: &body}
+	return &msg, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request, q queue.Stacker) {
+	if r.Method == "POST" {
+		msg, err := getMessageFromRequest(r)
+		if err != nil {
+			fmt.Println("Error trying to parse message from body", err)
+			w.WriteHeader(422) // unprocessable entity
+		} else {
+			err = q.AddToStack(msg)
+
+			if err != nil {
+				w.WriteHeader(422) // unprocessable entity
+				fmt.Println("Error:", err)
+			}
+			w.WriteHeader(200)
+		}
+	} else {
+		w.WriteHeader(405)
+	}
+}
---
+++ web/server_test.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/sayden/gubsub/queue"
+)
+
+type MockStacker struct{}
+
+func (m *MockStacker) AddToStack(msg *queue.Message) error {
+	return nil
+}
+
+func TestServerCanReceivePostRequests(t *testing.T) {
+	m := MockStacker{}
+	go Start(&m)
+	time.Sleep(1 * time.Second)
+
+	//Create a POST request, should return a 200
+	r, err := http.PostForm("http://localhost:12345/receive",
+		url.Values{"key": {"Value"}, "id": {"123"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r.StatusCode != 200 {
+		t.Fatal("Status code of server response was not 200")
+	}
+
+	//Create a GET request, should return a "not allowed"
+	r, err = http.Get("http://localhost:12345/receive")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r.StatusCode != 405 {
+		t.Fatal("Status code of server response was not 405")
+	}
+}
