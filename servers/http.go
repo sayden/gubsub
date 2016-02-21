4b5931a2be7c52a66bf6433b121828b435d4393c
Mario Castro <mariocaster@gmail.com>
2016-02-22 00:02:56 +0100

    Milestone of working version

--- servers/http.go
+++ servers/http.go
@@ -2,12 +2,48 @@ package servers
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/sayden/gubsub/dispatcher"
 )
 
-func StartHTTPServer(port int, messageHandler func(w http.ResponseWriter, r *http.Request)) {
+//StartHTTPServer will launch the http server
+func StartHTTPServer(port int) {
 	httpServer := http.NewServeMux()
-	httpServer.HandleFunc("/message", messageHandler)
+	httpServer.HandleFunc("/topic/default", httpMessageHandler)
 	println("Listening HTTP on port", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), httpServer)
+}
+
+//Parses a request to return a []byte object with the body content
+func getMessageFromRequest(r *http.Request) (*[]byte, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	if err := r.Body.Close(); err != nil {
+		return nil, err
+	}
+
+	return &body, nil
+}
+
+func httpMessageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		msg, err := getMessageFromRequest(r)
+		if err != nil {
+			fmt.Println("Error trying to parse message from body", err)
+			w.WriteHeader(422) // unprocessable entity
+		} else {
+			if err != nil {
+				w.WriteHeader(422) // unprocessable entity
+				fmt.Println("Error:", err)
+			}
+			dispatcher.Dispatch(msg)
+			w.WriteHeader(200)
+		}
+	} else {
+		w.WriteHeader(405)
+	}
 }
--- servers/socket.go
+++ servers/socket.go
@@ -4,12 +4,26 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/sayden/gubsub/dispatcher"
+	"github.com/sayden/gubsub/types"
+
 	"golang.org/x/net/websocket"
 )
 
-func StartSocketServer(port int, socketHandler func(ws *websocket.Conn)) {
+//StartSocketServer will launch the web socket server on the specified endpoint
+func StartSocketServer(port int, endpoint string) {
 	socket := http.NewServeMux()
-	socket.Handle("/websocket", websocket.Handler(socketHandler))
+	socket.Handle("/"+endpoint, websocket.Handler(func(ws *websocket.Conn) {
+		socketHandler(ws, endpoint)
+	}))
 	go func(socket *http.ServeMux) { http.ListenAndServe(fmt.Sprintf(":%d", port), socket) }(socket)
-	println("Listening Websocket on port 12345")
+	fmt.Printf("Listening Websocket on port %d \n", port)
+}
+
+func socketHandler(ws *websocket.Conn, endpoint string) {
+	c := make(chan *[]byte)
+	q := make(chan bool)
+	l := types.Listener{c, q, endpoint}
+
+	dispatcher.AddListener(ws, l)
 }
