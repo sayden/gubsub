99fb56b6cc7cf05b7c290b61008a6f8c4321731b
Mario Castro <mariocaster@gmail.com>
2016-02-24 00:43:59 +0100

    routing extracted to different file

--- servers/routes.go
+++ servers/routes.go
@@ -1,1 +1,35 @@
 package servers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sayden/gubsub/dispatcher"
+	"github.com/sayden/gubsub/types"
+)
+
+func GetAllTopics(c *gin.Context) {
+	ts := dispatcher.GetAllTopics()
+	c.JSON(http.StatusOK, gin.H{"result": ts})
+}
+
+func PostMessageOnTopic(c *gin.Context) {
+	name := c.Param("name")
+
+	var msg types.Message
+	err := c.BindWith(&msg, &types.MessageBinding{})
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"result": err.Error()})
+	} else {
+		msg.Topic = &name
+		msg.Timestamp = time.Now()
+		dispatcher.DispatchMessage(&msg)
+		c.JSON(http.StatusOK, gin.H{"result": "Message received"})
+	}
+}
+
+func GetAllListeners(c *gin.Context) {
+	ls := dispatcher.GetAllListeners()
+	c.JSON(http.StatusOK, gin.H{"result": ls})
+}
